3ceb516a5c5aa16c4e12303a5c2faba6f95fa7c3
mryhryki <mryhryki@gmail.com>
2023-04-19 13:37:32 +0900

    Update src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
+// https://auth0.com/docs/quickstart/webapp/nextjs#add-the-userprovider-component
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import {UserProvider} from '@auth0/nextjs-auth0/client';
+import type {AppProps} from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const log = (message: string) => {
+  const { stdout } = process
+  stdout.write(message)
+  stdout.end()
+}
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+}
+export const getServerSideProps = () => {
+  log('AAAAAAAAAAAAAAAAAa')
+  return {}
 }
