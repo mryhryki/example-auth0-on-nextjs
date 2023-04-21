7381c91a38040cadc31d75d11acbec370af9cd29
mryhryki <mryhryki@gmail.com>
2023-04-21 10:34:28 +0900

    Revert "Update src/pages/_app.tsx"
    This reverts commit 3ceb516a5c5aa16c4e12303a5c2faba6f95fa7c3.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,6 @@
-// https://auth0.com/docs/quickstart/webapp/nextjs#add-the-userprovider-component
 import '@/styles/globals.css'
-import {UserProvider} from '@auth0/nextjs-auth0/client';
-import type {AppProps} from 'next/app'
+import type { AppProps } from 'next/app'
 
-const log = (message: string) => {
-  const { stdout } = process
-  stdout.write(message)
-  stdout.end()
-}
-
-export default function App({Component, pageProps}: AppProps) {
-  return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
-  );
-}
-export const getServerSideProps = () => {
-  log('AAAAAAAAAAAAAAAAAa')
-  return {}
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
