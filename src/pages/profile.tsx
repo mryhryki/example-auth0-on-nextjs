39d17c12a4d6af3af4c067ecfdc81ae71f966643
mryhryki <mryhryki@gmail.com>
2023-04-25 10:06:50 +0900

    Update src/pages/profile.tsx

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -12,13 +12,17 @@ export default function Profile({user}: any /* FIXME */) {
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const serverSideProps = await withPageAuthRequired()(ctx);
   const user: UserProfile = (serverSideProps as any /* FIXME */).props.user
+  process.stdout.write(`### Server Side Log 1 ###\n${JSON.stringify(user, null, 2)}\n`)
+  const {hostname} = new URL(process.env.AUTH0_ISSUER_BASE_URL ?? "")
   const client = new ManagementClient({
     token: process.env.AUTH0_MANAGEMENT_API_TOKEN ?? "",
-    domain: new URL(process.env.AUTH0_ISSUER_BASE_URL ?? "").hostname,
+    domain: hostname,
   })
-  process.stdout.write(`### Server Side Log 1 ###\n${JSON.stringify(user, null, 2)}\n`)
   // @ts-ignore
-  const auth0User = await client.getUser({id: user.sub ?? "", fields: ["app_metadata"]})
+  const auth0User = await client.getUser({id: user.sub ?? ""})
   process.stdout.write(`### Server Side Log 2 ###\n${JSON.stringify(auth0User, null, 2)}\n`)
+  const userRoles = await client.getUserRoles({id: user.sub ?? ""})
+  const userPermissions = await client.getUserPermissions({id: user.sub ?? ""})
+  process.stdout.write(`### Server Side Log 3 ###\n${JSON.stringify({userRoles, userPermissions}, null, 2)}\n`)
   return serverSideProps
 }
