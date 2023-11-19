249d60386a3723973c8f3ceb6d5ad748ed64d838
mryhryki <mryhryki@gmail.com>
2023-11-20 08:26:33 +0900

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,10 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
-import {useUser} from "@auth0/nextjs-auth0/client";
-import Link from "next/link";
-import {withPageAuthRequired} from "@auth0/nextjs-auth0";
-import {useFetchTest} from "@/hooks/useFetchTest";
-import {useElapsedSec} from "@/hooks/useElapsedSec";
-import {useMemo} from "react";
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export default function Home() {
-  const {user} = useUser()
-  const {
-    fetchTest,
-    testResults,
-    lastRequestedUnixTime,
-    settings,
-    addSetting,
-    editSetting,
-    removeSetting
-  } = useFetchTest()
-  const elapsedSec = useElapsedSec(lastRequestedUnixTime)
-
-  const testResultJson = useMemo(() => {
-    return JSON.stringify(testResults, null, 2)
-  }, [testResults])
+  const { user } = useUser();
 
   return (
     <>
@@ -33,66 +15,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico"/>
       </Head>
       <main className={styles.main}>
-        <header className={styles.header}>
-          <span className={styles.userName}>{user?.email ?? '(Not logged in)'}</span>
-          {user != null ? (
-            <Link href="/api/auth/logout">Logout</Link>
-          ) : (
-            <Link href="/api/auth/login">Login</Link>
-          )}
-        </header>
-
-        <div className={styles.content}>
-          <ol>
-            {settings.map((setting) => (
-              <li key={setting.id} className={styles.settingElement}>
-                <code>ID:{setting.id}</code>
-                <div>
-                  <label>
-                    <span>RequestDelay:</span>
-                    <input
-                      type="number"
-                      className={styles.numberInput}
-                      value={setting.requestDelay}
-                      onChange={(event) => editSetting(setting.id, {requestDelay: parseInt(event.target.value, 10)})}
-                    />
-                    <span>sec</span>
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    <span>ApiExecuteDelay:</span>
-                    <input
-                      type="number"
-                      className={styles.numberInput}
-                      value={setting.apiExecuteDelay}
-                      onChange={(event) => editSetting(setting.id, {apiExecuteDelay: parseInt(event.target.value, 10)})}
-                    />
-                    <span>sec</span>
-                  </label>
-                </div>
-                <div className={styles.settingButtons}>
-                  <button onClick={() => removeSetting(setting.id)}>Remove</button>
-                </div>
-              </li>
-            ))}
-          </ol>
-          <div className={styles.actionButtons}>
-            <button onClick={addSetting}>Add Setting</button>
-            <button onClick={fetchTest} disabled={settings.length < 1}>Fetch Test</button>
-            <span>(Elapsed: {Math.floor(elapsedSec / 60)}:{(elapsedSec % 60).toString(10).padStart(2, '0')})</span>
-            <button onClick={() => navigator.clipboard.writeText(testResultJson)} disabled={settings.length < 1}>Copy Result</button>
-          </div>
-          <pre
-            onChange={() => undefined}
-            className={styles.testResult}
-          >
-            {testResultJson}
-          </pre>
-        </div>
+        <div>Logged in as <strong>{user?.email ?? "(Unknown)"}</strong></div>
+        <section>
+          <h2>User info</h2>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </section>
+        <section>
+          <a href="/api/auth/logout">Logout</a>
+        </section>
       </main>
     </>
-  )
+  );
 }
 
-export const getServerSideProps = withPageAuthRequired()
+export const getServerSideProps = withPageAuthRequired();
