55287e6b9e395be29b3a6f0392cd8ba9d811c25c
mryhryki <mryhryki@gmail.com>
2023-11-24 17:05:06 +0900

    Update src/pages/api/auth/[...auth0].ts

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,4 +1,15 @@
 // pages/api/auth/[...auth0].js
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+// Ref: https://github.com/auth0/nextjs-auth0/issues/701#issuecomment-1255350171
+// @ts-ignore
+const login = async (req, res) => {
+  await handleLogin(req, res, {
+    authorizationParams: {
+      invitation: req.query.invitation,
+      organization: req.query.organization,
+    },
+  });
+};
+
+export default handleAuth({ login });
