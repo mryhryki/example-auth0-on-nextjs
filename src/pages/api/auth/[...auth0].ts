8b3c153b0e1153ba9b3d267f4fa1418a3e40f1fb
mryhryki <mryhryki@gmail.com>
2023-12-21 13:14:12 +0900

    Update src/pages/api/auth/[...auth0].ts

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -4,11 +4,15 @@ import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 // Ref: https://github.com/auth0/nextjs-auth0/issues/701#issuecomment-1255350171
 // @ts-ignore
 const login = async (req, res) => {
+  const invitation: string | undefined = req.query.invitation;
+  const organization: string | undefined = req.query.organization;
+  const connection = organization != null ? undefined : "Username-Password-Authentication";
+
   await handleLogin(req, res, {
     authorizationParams: {
-      invitation: req.query.invitation,
-      organization: req.query.organization,
-      connection: req.query.connection,
+      invitation,
+      organization,
+      connection,
     },
   });
 };
