9c314d29935611f1b6a19ec949ed73426d011128
mryhryki <mryhryki@gmail.com>
2024-01-22 17:21:16 +0900

    Revert "[Experimental#2] Support multiple Auth0 Applications"
    This reverts commit abf71a26aee8dd519d36bdc28466e0152a7ef5a1.

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,34 +1,14 @@
 // pages/api/auth/[...auth0].js
-import { initAuth0 } from "@auth0/nextjs-auth0";
-import { NextApiRequest, NextApiResponse } from "next";
-
-function getAuth(req: NextApiRequest): ReturnType<typeof initAuth0> {
-  const switchAnotherApplication: boolean = req.query.switchAnotherApplication === "true";
-  if (switchAnotherApplication) {
-    return initAuth0({
-      clientID: process.env.AUTH0_CLIENT_ID_ANOTHER_APPLICATION,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET_ANOTHER_APPLICATION,
-    });
-  } else {
-    return initAuth0({});
-  }
-}
-
-function filterString(value: unknown): string | undefined {
-  if (typeof value === "string" && value.trim() !== "") {
-    return value.trim();
-  }
-  return undefined;
-}
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 // Ref: https://github.com/auth0/nextjs-auth0/issues/701#issuecomment-1255350171
-async function login(req: NextApiRequest, res: NextApiResponse) {
-  const invitation = filterString(req.query.invitation);
-  const organization = filterString(req.query.organization);
+// @ts-ignore
+const login = async (req, res) => {
+  const invitation: string | undefined = req.query.invitation;
+  const organization: string | undefined = req.query.organization;
   const connection = organization != null ? undefined : "Username-Password-Authentication";
 
-  const auth0 = getAuth(req);
-  await auth0.handleLogin(req, res, {
+  await handleLogin(req, res, {
     authorizationParams: {
       invitation,
       organization,
@@ -37,7 +17,4 @@ async function login(req: NextApiRequest, res: NextApiResponse) {
   });
 };
 
-export default function auth(req: NextApiRequest, res: NextApiResponse) {
-  const auth0 = getAuth(req);
-  return auth0.handleAuth({ login })(req, res);
-}
+export default handleAuth({ login });
