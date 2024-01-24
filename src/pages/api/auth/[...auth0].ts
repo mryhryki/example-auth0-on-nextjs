d97d7730556671dc99a9378b0910d57641f278d9
mryhryki <mryhryki@gmail.com>
2024-01-24 13:53:49 +0900

    Update src/pages/api/auth/[...auth0].ts

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,11 +1,12 @@
 // pages/api/auth/[...auth0].js
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { NextApiRequest, NextApiResponse } from "next";
 
 // Ref: https://github.com/auth0/nextjs-auth0/issues/701#issuecomment-1255350171
 // @ts-ignore
-const login = async (req, res) => {
-  const invitation: string | undefined = req.query.invitation;
-  const organization: string | undefined = req.query.organization;
+const login = async (req: NextApiRequest, res: NextApiResponse) => {
+  const invitation = filterString(req.query.invitation);
+  const organization = filterString(req.query.organization);
   const connection = organization != null ? undefined : "Username-Password-Authentication";
 
   await handleLogin(req, res, {
@@ -16,5 +17,9 @@ const login = async (req, res) => {
     },
   });
 };
+
+function filterString(val: unknown): string | undefined {
+  return typeof val === "string" ? val : undefined;
+}
 
 export default handleAuth({ login });
