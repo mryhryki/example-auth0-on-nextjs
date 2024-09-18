4eabb3d10e2fff2d385138152652566b59646681
mryhryki <mryhryki@gmail.com>
2024-09-18 11:53:27 +0900

    Improve error UI

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,5 +1,5 @@
 // pages/api/auth/[...auth0].js
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin, handleCallback } from "@auth0/nextjs-auth0";
 import { NextApiRequest, NextApiResponse } from "next";
 
 // Ref: https://github.com/auth0/nextjs-auth0/issues/701#issuecomment-1255350171
@@ -18,8 +18,36 @@ const login = async (req: NextApiRequest, res: NextApiResponse) => {
   });
 };
 
+const callback = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await handleCallback(req, res, {})
+  } catch (err: unknown) {
+    console.error('Auth0 callback error:',err)
+    res.writeHead(400, { "Content-Type": "text/html; charset=utf-8" });
+    const title = err instanceof Error ? err.message : "An error occurred";
+    res.send(`
+      <!doctype html>
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>${title}</title>
+      </head>
+      <body style="max-width: 40rem; margin: 1rem auto;">
+        <h1 style="font-size: 1.5rem;">${title}</h1>
+        <ul>${Object.entries(req.query).map(([key, value]): string => {
+            const values = Array.isArray(value) ? value : [value];
+            return `<li><strong>${key}:</strong><ul>${values.map((value) => `<li>${value}</li>`)}</ul></li>`;
+          }).join("")}</ul>
+      </body>
+      </html>
+    `);
+    res.end();
+    return;
+  }
+}
+
 function filterString(val: unknown): string | undefined {
   return typeof val === "string" ? val : undefined;
 }
 
-export default handleAuth({ login });
+export default handleAuth({ login, callback });
