089c5e320aa61034a63a2e382af7a929faf01232
mryhryki <mryhryki@gmail.com>
2024-09-18 12:29:52 +0900

    Add default layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,32 @@
 import '@/styles/globals.css'
-import type {AppProps} from 'next/app'
-import {UserProvider} from '@auth0/nextjs-auth0/client';
+import type { AppProps } from 'next/app'
+import { UserProvider, useUser } from '@auth0/nextjs-auth0/client'
+import styles from '@/pages/_app.module.css'
+import { FC } from 'react'
+import Link from 'next/link'
 
-export default function App({Component, pageProps}: AppProps) {
+const AppHeader: FC = () => {
+  const { user } = useUser()
+  const email = user?.email ?? '(Unknown email)'
+  return (<div className={styles.header}>
+    <div>{email}</div>
+    <Link href="/api/auth/logout">Logout</Link>
+  </div>)
+
+}
+
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <div className={styles.wrapper}>
+        <AppHeader />
+        <div className={styles.sideBar}>
+          <a className={styles.sideBarLink} href='/me'>My Info</a>
+        </div>
+        <div className={styles.content}>
+          <Component {...pageProps} />
+        </div>
+      </div>
     </UserProvider>
   )
 }
