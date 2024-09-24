e1b196f74c0dcbb2d70738a2482d556191506235
mryhryki <mryhryki@gmail.com>
2024-09-24 17:35:25 +0900

    Update src/components/layout/AppSideBar.tsx

--- src/components/layout/AppSideBar.tsx
+++ src/components/layout/AppSideBar.tsx
@@ -1,12 +1,24 @@
 import { FC } from 'react'
 import styles from '@/pages/_app.module.css'
 import Link from 'next/link'
+import { useCurrentUrl } from '@/hooks/useCurrentUrl'
+
+const Paths: string[] = [
+  '/access_info',
+  '/sso_configuration'
+]
 
 export const AppSideBar: FC = () => {
+  const currentUrl = useCurrentUrl()
+  const currentPath: string = currentUrl?.pathname?.split('/')?.slice(0, 2)?.join("/") ?? ''
+
   return (
     <div className={styles.sideBar}>
-      <Link href='/access_info'>Access Info</Link>
-      <Link href='/sso_configuration'>SSO Configuration</Link>
+      {Paths.map((path: string) => (
+        <Link key={path} href={path} className={path === currentPath ? 'active' : ''}>
+          {path.substring(1).split('_').map((s) => s).join(" ")}
+        </Link>
+      ))}
       <Link href="/api/auth/logout">Logout</Link>
     </div>
   )
