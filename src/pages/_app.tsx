3105e78ae3fe560f61e9601af18932b2d7ba2072
mryhryki <mryhryki@gmail.com>
2024-09-24 18:29:31 +0900

    Update src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
+import styles from '@/pages/_app.module.css'
+import { AppHeader } from '@/components/layout/AppHeader'
+import { AppSideBar } from '@/components/layout/AppSideBar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <UserProvider>
+      <div className={styles.wrapper}>
+        <AppHeader />
+        <AppSideBar />
+        <div className={styles.content}>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </UserProvider>
+  )
 }
