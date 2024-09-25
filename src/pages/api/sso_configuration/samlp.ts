f3c6c828d9b7203f9855cb3b34f0a8d51507aef2
mryhryki <mryhryki@gmail.com>
2024-09-25 16:21:10 +0900

    Update src/pages/api/sso_configuration/samlp.ts

--- src/pages/api/sso_configuration/samlp.ts
+++ src/pages/api/sso_configuration/samlp.ts
@@ -2,24 +2,27 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { withApiAuthRequired } from '@auth0/nextjs-auth0'
 import { auth0ManagementClient } from '@/utils/auth0'
 
-export const POST = withApiAuthRequired(async (
+export default withApiAuthRequired(async (
   req: NextApiRequest,
   res: NextApiResponse<unknown | { error: unknown }>,
 ) => {
   try {
-    console.debug('#####', JSON.stringify({ body: req.body }, null, 2))
+    const { displayName, signInUrl, x509SigningCert } = req.body
+    const date = new Date().toISOString().substring(0, 10).replace(/-/g, '')
+    const uuid = crypto.randomUUID()
 
     const response = await auth0ManagementClient.connections.create({
-      name: 'sso_name',
-      display_name: 'sso_display_name',
       strategy: 'samlp',
+      name: `${date}-${uuid}`,
+      display_name: displayName,
+      options: {
+        signInEndpoint: signInUrl,
+        signingCert: x509SigningCert,
+      },
     })
-    if (response.status === 200) {
-      res.status(200).json({ success: true })
-      return
-    }
-    res.status(response.status).json({ error: response })
+    res.status(200).json({ success: true, payload: response.data })
   } catch (err) {
-    res.status(500).json({ error: err })
+    console.error('ERROR:', err)
+    res.status(500).json({ success: false, error: err })
   }
 })
