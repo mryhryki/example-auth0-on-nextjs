4cfd4dfdfed9f60671c0e24108038b0e9c61457f
mryhryki <mryhryki@gmail.com>
2024-09-26 20:26:22 +0900

    Update src/hooks/useOrganization.ts

--- src/hooks/useOrganization.ts
+++ src/hooks/useOrganization.ts
@@ -1,6 +1,7 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { fetchApi } from '@/utils/api'
 import { Auth0Organization } from '@/pages/api/auth0/user_management_api_v2/organizations/get_organization'
+import { AppMessage } from '@/components/message/AppMessage'
 
 interface UseOrganizationState {
   organization: Auth0Organization | null
@@ -15,23 +16,27 @@ export const useOrganization = (): UseOrganizationState => {
   const [restrictedConnectionIds, setRestrictedConnectionIds] = useState<string[]>([])
 
   const reload = useCallback(async(): Promise<void> => {
-    await fetchApi<{ organization: Auth0Organization }>(
-      'GET',
-      '/auth0/user_management_api_v2/organizations/get_organization',
-    ).then((data) => {
-      setOrganization(data.organization)
-      setLoading(false)
-      try {
-        const ids = JSON.parse(data.organization.metadata?.restrictedConnectionIds ?? '[]')
-        if (Array.isArray(ids)) {
-          setRestrictedConnectionIds(ids)
-          return
+    try {
+      await fetchApi<{ organization: Auth0Organization }>(
+        'GET',
+        '/auth0/user_management_api_v2/organizations/get_organization',
+      ).then((data) => {
+        setOrganization(data.organization)
+        setLoading(false)
+        try {
+          const ids = JSON.parse(data.organization.metadata?.restrictedConnectionIds ?? '[]')
+          if (Array.isArray(ids)) {
+            setRestrictedConnectionIds(ids)
+            return
+          }
+          console.warn('restrictedConnectionIds is not an array:', data.organization.metadata?.restrictedConnectionIds)
+        } catch (err) {
+          console.warn(err)
         }
-        console.warn('restrictedConnectionIds is not an array:', data.organization.metadata?.restrictedConnectionIds)
-      } catch (err) {
-        console.warn(err)
-      }
-    })
+      })
+    } catch (err) {
+      AppMessage.addErrorMessage(`Failed to load organization: ${err}`)
+    }
   }, [])
 
   useEffect(() => {
