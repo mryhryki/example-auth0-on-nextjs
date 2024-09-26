15843af713dd726525210ea0b997a4e6da943a5a
mryhryki <mryhryki@gmail.com>
2024-09-26 09:45:48 +0900

    Update src/pages/access_info/index.tsx

--- src/pages/access_info/index.tsx
+++ src/pages/access_info/index.tsx
@@ -1,27 +1,8 @@
-import { getAccessToken, getSession, withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { GetServerSidePropsContext } from 'next'
+import { Auth0Session, getAuth0Session } from '@/utils/session'
 
-interface AccessInfoPageProps {
-  user: {
-    email: string
-    email_verified: boolean
-    sid: string
-    sub: string
-  }
-  organization: {
-    id: string
-    name: string
-    displayName: string
-    enableSSO: boolean
-  }
-  accessToken: {
-    hash: string;
-    scope: string;
-    expiresAt: number;
-  }
-}
-
-export default function AccessInfoPage(props: AccessInfoPageProps) {
+export default function AccessInfoPage(props: Auth0Session) {
   const { user, organization, accessToken } = props
 
   return (
@@ -38,10 +19,10 @@ export default function AccessInfoPage(props: AccessInfoPageProps) {
       <section>
         <h2>Organization</h2>
         <ul>
-          <li>ID: <strong>{organization.id}</strong></li>
-          <li>Name: <strong>{organization.name}</strong>
+          <li>ID: <strong>{organization.orgId}</strong></li>
+          <li>Name: <strong>{organization.orgName}</strong>
             <ul>
-              <li>Login URL: <a href={`/api/auth/login?organization=${organization.id}`}>Login</a></li>
+              <li>Login URL: <a href={`/api/auth/login?organization=${organization.orgId}`}>Login</a></li>
             </ul>
           </li>
           <li>Display name: <strong>{organization.displayName}</strong></li>
@@ -51,9 +32,9 @@ export default function AccessInfoPage(props: AccessInfoPageProps) {
       <section>
         <h2>Access Token</h2>
         <ul>
-        <li>Hash: <strong>{accessToken.hash}</strong></li>
-          <li>Scope: <strong>{accessToken.scope}</strong></li>
-          <li>Expires: <strong>{new Date(accessToken.expiresAt * 1000).toISOString()}</strong></li>
+        <li>Hash: <strong>{accessToken.accessTokenHash}</strong></li>
+          <li>Scope: <strong>{accessToken.accessTokenScope}</strong></li>
+          <li>Expires: <strong>{new Date(accessToken.accessTokenExpiresAt * 1000).toISOString()}</strong></li>
         </ul>
       </section>
     </>
@@ -62,37 +43,7 @@ export default function AccessInfoPage(props: AccessInfoPageProps) {
 
 export const getServerSideProps = withPageAuthRequired({
   getServerSideProps: async function getServerSideProps(ctx: GetServerSidePropsContext) {
-    const session = await getSession(ctx.req, ctx.res)
-    // console.debug('Session:', JSON.stringify(session, null, 2))
-    const { user, accessTokenScope, accessTokenExpiresAt } = session ?? {}
-
-    const accessToken: string = (await getAccessToken(ctx.req, ctx.res, { refresh: false })).accessToken ?? ''
-    // console.debug('Access token:', accessToken)
-
-    const accessTokenHash = Buffer.from(
-      await crypto.subtle.digest('SHA-256', new TextEncoder().encode(accessToken)),
-    ).toString('hex')
-
-    const props: AccessInfoPageProps = {
-      user: {
-        email: user?.email ?? '(No email)',
-        email_verified: user?.email_verified ?? false,
-        sid: user?.sid ?? '(No sid)',
-        sub: user?.sub ?? '(No sub)',
-      },
-      organization: {
-        id: user?.org_id ?? '(No org_id)',
-        name: user?.org_name ?? '(No org_name)',
-        displayName: user?.orgDisplayName ?? '(No orgDisplayName)',
-        enableSSO: user?.orgEnableSSO ?? false,
-      },
-      accessToken: {
-        hash: accessTokenHash,
-        scope: accessTokenScope ?? "(No scope)",
-        expiresAt: accessTokenExpiresAt ?? 0,
-      }
-    }
-
+    const props = await getAuth0Session(ctx.req, ctx.res)
     return { props }
   },
 })
