a9150d5111921109ff55d5ce36b90ed5eeda69c5
mryhryki <mryhryki@gmail.com>
2024-09-26 10:55:40 +0900

    Update src/hooks/useCurrentUrl.ts

--- src/hooks/useCurrentUrl.ts
+++ src/hooks/useCurrentUrl.ts
@@ -1,12 +1,19 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export const useCurrentUrl = (): URL | null => {
+  const beforeUrl = useRef<string>('init')
   const [currentUrl, setCurrentUrl] = useState<URL | null>(null)
 
   useEffect(() => {
     const _window = globalThis.window
     if (_window == null) return
-    setCurrentUrl(new URL(_window.location.href))
+    const intervalId = setInterval(() => {
+      const url = new URL(_window.location.href)
+      if (url.toString() === beforeUrl.current) return
+      beforeUrl.current = url.toString()
+      setCurrentUrl(url)
+    }, 200)
+    return () => clearInterval(intervalId)
   }, [])
 
   return currentUrl
