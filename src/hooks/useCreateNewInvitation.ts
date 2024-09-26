f0c8248ea5d5a72847ad918dfdae2a9a21e77805
mryhryki <mryhryki@gmail.com>
2024-09-26 14:07:36 +0900

    Update src/hooks/useCreateNewInvitation.ts

--- src/hooks/useCreateNewInvitation.ts
+++ src/hooks/useCreateNewInvitation.ts
@@ -1,5 +1,6 @@
 import { Dispatch, FormEvent, SetStateAction, useState } from 'react'
 import { AddMessage } from '@/hooks/useMessages'
+import { fetchApi } from '@/utils/api'
 
 interface FormValues {
   connectionId: string;
@@ -20,28 +21,20 @@ interface UseCreateNewInvitationState {
 export const useCreateNewInvitation = (args: UseCreateNewInvitationArgs): UseCreateNewInvitationState => {
   const [values, setValues] = useState<FormValues>({ connectionId: '', email: '' })
 
-  const canSubmit: boolean = values.email.trim().length > 0 && values.email.includes('@')
+  const canSubmit: boolean = values.connectionId.trim().length > 0 &&
+                             values.email.trim().length > 0 &&
+                             values.email.includes('@')
   const onSubmit = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
     if (!canSubmit) return
     const { connectionId, email } = values
 
-    const response = await fetch(`/api/invitation/new`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify({ connectionId, email }),
-    })
-    if (response.ok) {
-      const body = await response.text()
-      args.addMessage('info', `Invitation has been created: ${body}`)
-    } else {
-      const { status } = response
-      const body = await response.text()
-      const errorMessage = `Failed to create an invitation: ${status} ${body}`
-      args.addMessage('error', errorMessage)
-    }
+    const response = await fetchApi(
+      'POST',
+      '/auth0/user_management_api_v2/organizations/create_invitation',
+      { connectionId, email },
+    )
+    args.addMessage('info', `Invitation has been created: ${JSON.stringify(response)}`)
   }
 
   return {
