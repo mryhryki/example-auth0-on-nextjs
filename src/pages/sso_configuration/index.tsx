a0c39452fe50e5d1d817e7061e481e1341f333b9
mryhryki <mryhryki@gmail.com>
2024-09-26 09:51:12 +0900

    Update src/pages/sso_configuration/index.tsx

--- src/pages/sso_configuration/index.tsx
+++ src/pages/sso_configuration/index.tsx
@@ -1,7 +1,8 @@
-import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { GetServerSidePropsContext } from 'next'
 import { auth0ManagementClient } from '@/utils/auth0/client'
 import Link from 'next/link'
+import { Auth0OrganizationInfo, getAuth0Session } from '@/utils/session'
 
 interface OrganizationConnection {
   connection_id: string;
@@ -13,19 +14,13 @@ interface OrganizationConnection {
 }
 
 interface SsoConfigurationIndexPageProps {
-  organization: {
-    id: string
-    name: string
-    displayName: string
-    enableSSO: boolean
-  }
+  organization: Auth0OrganizationInfo
   connections: OrganizationConnection[]
 }
 
 export default function SsoConfigurationIndexPage(props: SsoConfigurationIndexPageProps) {
   const { organization, connections } = props
-  const { displayName, enableSSO } = organization
-  const orgIdWithoutPrefix = organization.id.substring('org_'.length)
+  const { orgIdWithoutPrefix, displayName, enableSSO } = organization
 
   return (
     <section>
@@ -49,7 +44,7 @@ export default function SsoConfigurationIndexPage(props: SsoConfigurationIndexPa
         </>
       ) : (
         <p>
-          <strong>{displayName}</strong> (Name: <strong>{organization.name}</strong>) organization is not enabled SSO.
+          <strong>{displayName}</strong> (Name: <strong>{organization.orgName}</strong>) organization is not enabled SSO.
         </p>
       )}
     </section>
@@ -58,27 +53,17 @@ export default function SsoConfigurationIndexPage(props: SsoConfigurationIndexPa
 
 export const getServerSideProps = withPageAuthRequired({
   getServerSideProps: async function getServerSideProps(ctx: GetServerSidePropsContext) {
-    const session = await getSession(ctx.req, ctx.res)
-    // console.debug('Session:', JSON.stringify(session, null, 2))
-    const { user } = session ?? {}
-
-    const orgId = user?.org_id ?? '(No org_id)'
-    const enableSSO = user?.orgEnableSSO ?? false
+    const session = await getAuth0Session(ctx.req, ctx.res)
 
     const { data } = await auth0ManagementClient.organizations.getEnabledConnections({
-      id: orgId,
+      id: session.organization.orgId,
       page: 0,
       per_page: 100,
       include_totals: true,
     })
 
     const props: SsoConfigurationIndexPageProps = {
-      organization: {
-        id: orgId,
-        name: user?.org_name ?? '(No org_name)',
-        displayName: user?.orgDisplayName ?? '(No orgDisplayName)',
-        enableSSO,
-      },
+      organization: session.organization,
       connections: data.enabled_connections,
     }
     return { props }
