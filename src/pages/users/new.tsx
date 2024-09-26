ff3b13683139c0578cfcb44bc38c26bcba98940b
mryhryki <mryhryki@gmail.com>
2024-09-26 14:07:36 +0900

    Update src/pages/users/new.tsx

--- src/pages/users/new.tsx
+++ src/pages/users/new.tsx
@@ -1,23 +1,16 @@
-import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { Messages } from '@/components/message/Messages'
 import { useMessages } from '@/hooks/useMessages'
 import { useCreateNewInvitation } from '@/hooks/useCreateNewInvitation'
-import { GetServerSidePropsContext } from 'next'
-import { auth0ManagementClient } from '@/utils/auth0/client'
+import { useOrganizationConnections } from '@/hooks/useOrganizationConnections'
+import { Auth0Session, getServerSidePropsForSession } from '@/utils/session'
+import { Loading } from '@/components/loading/Loading'
 
-interface Connection {
-  id: string
-  name: string
-}
-
-interface UsersNewPageProps {
-  connections: Connection[];
-}
-
-export default function UsersNewPage(props: UsersNewPageProps) {
-  const {connections} = props
+export default function UsersNewPage(props: Auth0Session) {
+  const { organization: { orgIdWithoutPrefix } } = props
   const { errorMessages, removeMessage, addMessage } = useMessages()
   const { values, setValues, canSubmit, onSubmit } = useCreateNewInvitation({ addMessage })
+  const { connections, loading } = useOrganizationConnections()
 
   return (
     <section>
@@ -25,14 +18,21 @@ export default function UsersNewPage(props: UsersNewPageProps) {
       <form onSubmit={onSubmit}>
         <label>
           <div>Connection</div>
-          <select
-            value={values.connectionId}
-            onChange={(event) => setValues((prev) => ({ ...prev, connectionId: event.target.value }))}
-          >
-            {connections.map((connection) => (
-              <option key={connection.id} value={connection.id}>{connection.name}</option>
-            ))}
-          </select>
+          {loading ? <Loading /> : (
+            <select
+              value={values.connectionId}
+              onChange={(event) => setValues((prev) => ({ ...prev, connectionId: event.target.value }))}
+            >
+              {connections.map((connection) => (
+                <option
+                  key={connection.connection_id}
+                  value={connection.connection_id}
+                >
+                  {connection.connection.name.replace(new RegExp(`^${orgIdWithoutPrefix}-`), '')}
+                </option>
+              ))}
+            </select>
+          )}
         </label>
         <label>
           <div>Email</div>
@@ -50,29 +50,6 @@ export default function UsersNewPage(props: UsersNewPageProps) {
 }
 
 export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async function getServerSideProps(ctx: GetServerSidePropsContext) {
-    const session = await getSession(ctx.req, ctx.res)
-    // console.debug('Session:', JSON.stringify(session, null, 2))
-    const { user } = session ?? {}
-
-    const orgId = user?.org_id ?? '(No org_id)'
-
-    const { data } = await auth0ManagementClient.organizations.getEnabledConnections({
-      id: orgId,
-      page: 0,
-      per_page: 100,
-      include_totals: true,
-    })
-
-    const orgIdWithoutPrefix = orgId.substring('org_'.length)
-
-    const props: UsersNewPageProps = {
-      connections: data.enabled_connections.map((connection): Connection => ({
-        id: connection.connection_id ?? '(No connection_id)',
-        name: connection.connection.name?.replace(new RegExp(`^${orgIdWithoutPrefix}-`), '') ?? '(No name)',
-      })),
-    }
-    return { props }
-  },
+  getServerSideProps: getServerSidePropsForSession,
 })
 
