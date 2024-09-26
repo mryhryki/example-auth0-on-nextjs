ed46fe1640a512e4df614e3153df545def5ed48d
mryhryki <mryhryki@gmail.com>
2024-09-26 13:44:40 +0900

    Update src/pages/users/index.tsx

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,96 +1,50 @@
-import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0'
-import { GetServerSidePropsContext } from 'next'
-import { auth0ManagementClient } from '@/utils/auth0/client'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import Link from 'next/link'
+import { useOrganizationMembers } from '@/hooks/useOrganizationMembers'
+import { Loading } from '@/components/loading/Loading'
+import { useOrganizationInvitations } from '@/hooks/useOrganizationInvitations'
 
-interface OrganizationMember {
-  user_id?: string | undefined;
-  picture?: string | undefined;
-  name?: string | undefined;
-  email?: string | undefined;
-}
-
-interface OrganizationInvitation {
-  id: string;
-  organization_id: string;
-  inviter: {
-    name: string;
-  };
-  invitee: {
-    email: string;
-  };
-  invitation_url: string;
-  created_at: string;
-  expires_at: string;
-  connection_id?: string | undefined;
-  client_id: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  app_metadata?: any;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  user_metadata?: any;
-  ticket_id: string;
-  roles?: string[] | undefined;
-}
-
-
-interface UsersPageProps {
-  users: OrganizationMember[]
-  invitations: OrganizationInvitation[]
-}
-
-export default function UsersPage(props: UsersPageProps) {
-  const { users, invitations } = props
+export default function UsersPage() {
+  const { members, loading: loadingMembers } = useOrganizationMembers()
+  const { invitations, loading: loadingInvitations } = useOrganizationInvitations()
 
   return (
     <>
       <h1>Users</h1>
       <section>
         <h2>1. Users</h2>
-        <ol>
-          {users.map((user, i) => (
-            <li key={user.user_id ?? i.toString()}>
-              <strong>{user.email ?? '(No email)'}</strong>
-              (<strong>{user.user_id ?? 'No user_id'}</strong>)
-            </li>
-          ))}
+        {loadingMembers ? <Loading /> : (
+          <ol>
+            {members.map((member, i) => (
+              <li key={member.user_id ?? i.toString()}>
+                <strong>{member.email ?? '(No email)'}</strong>
+                (<strong>{member.user_id ?? 'No user_id'}</strong>)
+              </li>
+            ))}
         </ol>
+        )}
       </section>
       <section>
         <h2>2. Invitations</h2>
-        <ol>
-          {invitations.map((invitation) => (
-            <li key={invitation.id}>
-              <strong>{invitation.invitee.email}</strong>
-              <ul>
-                <li>Connection: <strong>{invitation.connection_id ?? '(No connection)'}</strong></li>
-                <li>Created: <strong>{invitation.created_at}</strong></li>
-                <li>Expires: <strong>{invitation.expires_at}</strong></li>
-              </ul>
-            </li>
-          ))}
-        </ol>
         <Link href="/users/new">Create an invitation</Link>
+        {loadingInvitations ? <Loading /> : (
+          <ol>
+            {invitations.map((invitation) => (
+              <li key={invitation.id}>
+                <strong>{invitation.invitee.email}</strong>
+                <ul>
+                  <li>Connection: <strong>{invitation.connection_id ?? '(No connection)'}</strong></li>
+                  <li>Created: <strong>{invitation.created_at}</strong></li>
+                  <li>Expires: <strong>{invitation.expires_at}</strong></li>
+                </ul>
+              </li>
+            ))}
+          </ol>
+        )}
       </section>
     </>
   )
 }
 
-export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async function getServerSideProps(ctx: GetServerSidePropsContext) {
-    const session = await getSession(ctx.req, ctx.res)
-    // console.debug('Session:', JSON.stringify(session, null, 2))
-    const { user } = session ?? {}
-    const orgId: string = user?.org_id ?? '(No org_id)'
-
-    const { data: users } = await auth0ManagementClient.organizations.getMembers({ id: orgId, page: 0, per_page: 100 })
-    const { data: invitations } = await auth0ManagementClient.organizations.getInvitations({
-      id: orgId,
-      page: 0,
-      per_page: 100,
-    })
-
-    const props: UsersPageProps = { users, invitations }
-    return { props }
-  },
-})
+export const getServerSideProps = withPageAuthRequired({})
 
