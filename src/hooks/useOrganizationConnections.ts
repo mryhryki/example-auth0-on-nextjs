b414add970bd16bb1d5b33a7af38fbd0648b4184
mryhryki <mryhryki@gmail.com>
2024-09-26 10:20:06 +0900

    Add src/hooks/useOrganizationConnections.ts

---
+++ src/hooks/useOrganizationConnections.ts
@@ -0,0 +1,30 @@
+import {
+  Auth0OrganizationConnection,
+} from '@/pages/api/auth0/user_management_api_v2/organizations/get_enabled_connections'
+import { useEffect, useRef, useState } from 'react'
+import { fetchApi } from '@/utils/api'
+
+interface UseOrganizationConnectionsState {
+  connections: Auth0OrganizationConnection[]
+  loading: boolean;
+}
+
+export const useOrganizationConnections = (): UseOrganizationConnectionsState => {
+  const loading = useRef(true)
+  const [connections, setConnections] = useState<Auth0OrganizationConnection[]>([])
+
+  useEffect(() => {
+    fetchApi<{ connections: Auth0OrganizationConnection[] }>(
+      'GET',
+      '/auth0/user_management_api_v2/organizations/get_enabled_connections',
+    ).then((data) => {
+      loading.current = false
+      setConnections(data.connections)
+    })
+  }, [])
+
+  return {
+    connections,
+    loading: loading.current,
+  }
+}
