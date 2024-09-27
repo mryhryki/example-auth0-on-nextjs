406f78df212b370db80d2b53406b5f40218ac694
mryhryki <mryhryki@gmail.com>
2024-09-27 13:58:38 +0900

    Update src/pages/api/auth/handlers/login.ts

--- src/pages/api/auth/handlers/login.ts
+++ src/pages/api/auth/handlers/login.ts
@@ -1,21 +1,32 @@
-import { handleLogin } from "@auth0/nextjs-auth0";
+import { handleLogin } from '@auth0/nextjs-auth0'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { filterString } from '@/utils/string'
 
 export const auth0LoginHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Ref: https://github.com/auth0/nextjs-auth0/issues/701#issuecomment-1255350171
   const invitation = filterString(req.query.invitation);
-  const organization = filterString(req.query.organization);
-  const connection = organization != null ? filterString(req.query.connection) : "Username-Password-Authentication";
+  const organizationByQuery = filterString(req.query.organization)
+  const connectionByQuery = filterString(req.query.connection)
+
+  const organizationByCookie = filterString(req.cookies['auth0-organization'])
+  const connectionByCookie = filterString(req.cookies['auth0-connection'])
+
+  if (organizationByQuery == null &&
+      connectionByQuery == null &&
+      organizationByCookie != null &&
+      connectionByCookie != null) {
+    return res.redirect(
+      307,
+      `/select_login_method?organization=${organizationByCookie}&connection=${connectionByCookie}`,
+    )
+  }
 
   await handleLogin(req, res, {
     authorizationParams: {
-      invitation,
-      organization,
-      connection,
+      invitation: invitation ?? undefined,
+      organization: organizationByQuery ?? undefined,
+      connection: connectionByQuery ?? 'Username-Password-Authentication',
     },
   });
 };
 
-function filterString(val: unknown): string | undefined {
-  return typeof val === "string" && val.trim().length > 0 ? val : undefined;
-}
