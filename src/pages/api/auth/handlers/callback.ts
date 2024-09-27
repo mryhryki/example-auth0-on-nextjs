13bf492ab7e3e23d720da8c359247f6a8427e6da
mryhryki <mryhryki@gmail.com>
2024-09-27 14:18:14 +0900

    Update src/pages/api/auth/handlers/callback.ts

--- src/pages/api/auth/handlers/callback.ts
+++ src/pages/api/auth/handlers/callback.ts
@@ -1,9 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { handleCallback } from '@auth0/nextjs-auth0'
+import { handleCallback, Session } from '@auth0/nextjs-auth0'
 
 export const auth0CallbackHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    await handleCallback(req, res, {})
+    await handleCallback(req, res, {
+      afterCallback: async (_req: NextApiRequest, res: NextApiResponse, session: Session): Promise<Session> => {
+        const { user: { org_name: organization, loginConnectionName: connection } } = session
+        // Ref: https://stackoverflow.com/a/76357138
+        res.appendHeader('Set-Cookie', `auth0-organization=${organization}; HttpOnly; SameSite=Strict; Path=/; Max-Age=${86400 * 365}`)
+        res.appendHeader('Set-Cookie', `auth0-connection=${connection}; HttpOnly; SameSite=Strict; Path=/; Max-Age=${86400 * 365}`)
+        return session;
+      },
+    })
   } catch (err: unknown) {
     console.error('Auth0 callback error:',err)
 
