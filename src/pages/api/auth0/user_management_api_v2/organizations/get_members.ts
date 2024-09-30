150259b5c66386d94a6be9f8d57c45db42cc0c6d
mryhryki <mryhryki@gmail.com>
2024-09-30 12:02:46 +0900

    Update src/pages/api/auth0/user_management_api_v2/organizations/get_members.ts

--- src/pages/api/auth0/user_management_api_v2/organizations/get_members.ts
+++ src/pages/api/auth0/user_management_api_v2/organizations/get_members.ts
@@ -1,6 +1,8 @@
 import { auth0ManagementClient } from '@/utils/auth0/client'
 import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Auth0User } from '@/pages/api/auth0/user_management_api_v2/user/create_user'
+import { ApiResponse } from '@/pages/api/auth0/common'
 
 
 export interface Auth0OrganizationMember {
@@ -8,48 +10,6 @@ export interface Auth0OrganizationMember {
   picture?: string | undefined;
   name?: string | undefined;
   email?: string | undefined;
-}
-
-interface Auth0User {
-  user_id: string;
-  email: string;
-  email_verified: boolean;
-  username: string;
-  phone_number: string;
-  phone_verified: boolean;
-  created_at: string | { [key: string]: unknown; };
-  updated_at: string | { [key: string]: unknown; };
-  identities: {
-    connection: string;
-    user_id: string;
-    provider: string;
-    isSocial: boolean;
-    access_token: string;
-    access_token_secret: string;
-    refresh_token: string;
-    profileData: {
-      email?: string;
-      email_verified?: boolean;
-      name?: string;
-      username?: string;
-      given_name?: string;
-      phone_number?: string;
-      phone_verified?: boolean;
-      family_name?: string;
-    }
-  }[];
-  app_metadata: Record<string, unknown>
-  user_metadata: Record<string, unknown>
-  picture: string;
-  name: string;
-  nickname: string;
-  multifactor: Array<string>;
-  last_ip: string;
-  last_login: string | { [key: string]: unknown; };
-  logins_count: number;
-  blocked: boolean;
-  given_name: string;
-  family_name: string;
 }
 
 export interface Auth0OrganizationMemberWithRawUserData extends Auth0OrganizationMember {
@@ -62,7 +22,7 @@ interface ApiResponseData {
 
 export default withApiAuthRequired(async (
   req: NextApiRequest,
-  res: NextApiResponse<ApiResponseData | { error: unknown }>,
+  res: NextApiResponse<ApiResponse<ApiResponseData>>,
 ) => {
   try {
     const session = (await getSession(req, res)) ?? null
@@ -84,8 +44,8 @@ export default withApiAuthRequired(async (
       ...member,
       rawUserData: usersById.get(member.user_id) ?? null,
     }))
-    res.status(200).json({ members })
+    res.status(200).json({ success: true, payload: { members } })
   } catch (err) {
-    res.status(500).json({ error: err })
+    res.status(500).json({ success: false, error: err })
   }
 })
