6c8146f8df0a58b8e83586ce56d8a8f7aae704a3
mryhryki <mryhryki@gmail.com>
2024-09-30 11:43:34 +0900

    Rename src/pages/organization_members/invite.tsx

---
+++ src/pages/organization_members/invite.tsx
@@ -0,0 +1,56 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { useCreateNewInvitation } from '@/hooks/useCreateNewInvitation'
+import { Loading } from '@/components/loading/Loading'
+import { useOrganization } from '@/hooks/useOrganization'
+import { useEffect } from 'react'
+
+export default function UsersNewPage() {
+  const { values, setValues, canSubmit, onSubmit } = useCreateNewInvitation()
+  const { loading, connectionsByOrganizationMetadata } = useOrganization()
+
+  useEffect(() => {
+    if (connectionsByOrganizationMetadata.length === 0) return
+    setValues((prev) => ({ ...prev, connectionId: connectionsByOrganizationMetadata[0].connectionId }))
+  }, [connectionsByOrganizationMetadata, setValues])
+
+  return (
+    <section>
+      <h1>Create an Invitation</h1>
+      <form onSubmit={onSubmit}>
+        <label>
+          <div>Connection</div>
+          {loading ? <Loading /> : (
+            <select
+              value={values.connectionId}
+              onChange={(event) => setValues((prev) => ({ ...prev, connectionId: event.target.value }))}
+            >
+              {connectionsByOrganizationMetadata
+                .filter(({ isDatabaseConnection }) => !isDatabaseConnection)
+                .map((connection) => (
+                <option
+                  key={connection.connectionId}
+                  value={connection.connectionId}
+                >
+                  {connection.displayName}
+                </option>
+                ))
+              }
+            </select>
+          )}
+        </label>
+        <label>
+          <div>Email</div>
+          <input
+            type="text"
+            value={values.email}
+            onChange={(event) => setValues((prev) => ({ ...prev, email: event.target.value }))}
+          />
+        </label>
+        <input type="submit" value="Create" disabled={!canSubmit} />
+      </form>
+    </section>
+  )
+}
+
+export const getServerSideProps = withPageAuthRequired({})
+
