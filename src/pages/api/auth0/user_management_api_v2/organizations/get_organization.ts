9c551eeb6dba2c957376b217aa1331e0bf17ab22
mryhryki <mryhryki@gmail.com>
2024-09-30 12:02:47 +0900

    Update src/pages/api/auth0/user_management_api_v2/organizations/get_organization.ts

--- src/pages/api/auth0/user_management_api_v2/organizations/get_organization.ts
+++ src/pages/api/auth0/user_management_api_v2/organizations/get_organization.ts
@@ -1,6 +1,7 @@
 import { auth0ManagementClient } from '@/utils/auth0/client'
 import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { ApiResponse } from '@/pages/api/auth0/common'
 
 export interface Auth0Organization {
   id: string;
@@ -24,13 +25,13 @@ interface ApiResponseData {
 
 export default withApiAuthRequired(async (
   req: NextApiRequest,
-  res: NextApiResponse<ApiResponseData | { error: unknown }>,
+  res: NextApiResponse<ApiResponse<ApiResponseData>>,
 ) => {
   try {
     const session = (await getSession(req, res)) ?? null
     const { data } = await auth0ManagementClient.organizations.get({ id: session?.user.org_id })
-    res.status(200).json({ organization: data })
+    res.status(200).json({ success: true, payload: { organization: data }})
   } catch (err) {
-    res.status(500).json({ error: err })
+    res.status(500).json({ success: false, error: err })
   }
 })
