ae9fecfba6b809f736d1f01d665ba77b615b8c09
mryhryki <mryhryki@gmail.com>
2024-09-30 14:48:30 +0900

    Update src/pages/api/auth0/common.ts

--- src/pages/api/auth0/common.ts
+++ src/pages/api/auth0/common.ts
@@ -1,3 +1,6 @@
+import { getSession } from '@auth0/nextjs-auth0'
+import { NextApiRequest, NextApiResponse } from 'next'
+
 export type ApiSuccessResponse<T> = {
   success: true
   payload: T
@@ -8,5 +11,14 @@ export type ApiErrorResponse = {
   error: unknown
 }
 
-
 export type ApiResponse<T> = ApiSuccessResponse<T> | ApiErrorResponse
+
+export const getOrganizationId = async (req: NextApiRequest, res: NextApiResponse): Promise<string> => {
+  const session = await getSession(req, res)
+  const { user } = session ?? {}
+  const orgId = user?.org_id
+  if (typeof orgId !== 'string' || !orgId.startsWith('org_')) {
+    throw new Error('org_id is invalid')
+  }
+  return orgId;
+}
