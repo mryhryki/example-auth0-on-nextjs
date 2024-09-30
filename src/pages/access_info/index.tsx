93ab71b80f1f1968886d4386c8b0c8908e2c5a1e
mryhryki <mryhryki@gmail.com>
2024-09-30 11:38:58 +0900

    Update src/pages/access_info/index.tsx

--- src/pages/access_info/index.tsx
+++ src/pages/access_info/index.tsx
@@ -9,27 +9,27 @@ export default function AccessInfoPage(props: Auth0Session) {
       <section>
         <h2>User info</h2>
         <ul>
-          <li>Email: <strong>{user.email}</strong></li>
-          <li>Email verified: <strong>{user.email_verified.toString()}</strong></li>
-          <li>SID: <strong>{user.sid}</strong></li>
-          <li>Sub: <strong>{user.sub}</strong></li>
+          <li>Email:<strong>{user.email}</strong></li>
+          <li>Email verified:<strong>{user.email_verified.toString()}</strong></li>
+          <li>SID:<strong>{user.sid}</strong></li>
+          <li>Sub:<strong>{user.sub}</strong></li>
         </ul>
       </section>
       <section>
         <h2>Organization</h2>
         <ul>
-          <li>ID: <strong>{organization.orgId}</strong></li>
-          <li>Name: <strong>{organization.orgName}</strong></li>
-          <li>Display name: <strong>{organization.displayName}</strong></li>
-          <li>Enable SSO: <strong>{organization.enableSSO ? 'true' : 'false'}</strong></li>
+          <li>ID:<strong>{organization.orgId}</strong></li>
+          <li>Name:<strong>{organization.orgName}</strong></li>
+          <li>Display name:<strong>{organization.displayName}</strong></li>
+          <li>Enable SSO:<strong>{organization.enableSSO ? 'true' : 'false'}</strong></li>
         </ul>
       </section>
       <section>
         <h2>Access Token</h2>
         <ul>
-        <li>Hash: <strong>{accessToken.accessTokenHash}</strong></li>
-          <li>Scope: <strong>{accessToken.accessTokenScope}</strong></li>
-          <li>Expires: <strong>{new Date(accessToken.accessTokenExpiresAt * 1000).toISOString()}</strong></li>
+        <li>Hash:<strong>{accessToken.accessTokenHash}</strong></li>
+          <li>Scope:<strong>{accessToken.accessTokenScope}</strong></li>
+          <li>Expires:<strong>{new Date(accessToken.accessTokenExpiresAt * 1000).toISOString()}</strong></li>
         </ul>
       </section>
     </>
