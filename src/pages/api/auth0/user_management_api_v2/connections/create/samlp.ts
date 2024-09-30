97f663b61e7881141d4b00cc199e999afa4f4b5a
mryhryki <mryhryki@gmail.com>
2024-09-30 14:48:30 +0900

    Update src/pages/api/auth0/user_management_api_v2/connections/create/samlp.ts

--- src/pages/api/auth0/user_management_api_v2/connections/create/samlp.ts
+++ src/pages/api/auth0/user_management_api_v2/connections/create/samlp.ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { withApiAuthRequired } from '@auth0/nextjs-auth0'
 import { auth0ManagementClient } from '@/utils/auth0/client'
 import { getConnectionByOrganizationMetadata } from '@/utils/auth0/getConnectionByOrganizationMetadata'
-import { ApiResponse } from '@/pages/api/auth0/common'
+import { ApiResponse, getOrganizationId } from '@/pages/api/auth0/common'
 
 const MAX_CONNECTIONS = 2
 const OrgIdPrefix = 'org_'
@@ -17,15 +17,9 @@ export default withApiAuthRequired(async (
   res: NextApiResponse<ApiResponse<ApiResponseData>>,
 ) => {
   try {
-    const session = await getSession(req, res)
-    const { user } = session ?? {}
-    const orgId = user?.org_id
+    const organizationId = await getOrganizationId(req, res)
 
-    if (typeof orgId !== 'string' || !orgId.trim().startsWith(OrgIdPrefix)) {
-      return res.status(500).json({ success: false, error: 'org_id is invalid' })
-    }
-
-    const { data: organization } = await auth0ManagementClient.organizations.get({ id: orgId })
+    const { data: organization } = await auth0ManagementClient.organizations.get({ id: organizationId })
     const connections = getConnectionByOrganizationMetadata(organization.metadata)
     if (connections.length >= MAX_CONNECTIONS) {
       return res.status(403).json({ success: false, error: `Max connection(${MAX_CONNECTIONS}) reached` })
@@ -34,7 +28,7 @@ export default withApiAuthRequired(async (
     const { name, displayName, signInUrl, x509SigningCert } = req.body
     const { data: { id: connectionId, name: connectionName } } = await auth0ManagementClient.connections.create({
       strategy: 'samlp',
-      name: `${orgId.substring(OrgIdPrefix.length)}-${name}`,
+      name: `${organizationId.substring(OrgIdPrefix.length)}-${name}`,
       display_name: displayName,
       options: {
         signInEndpoint: signInUrl,
@@ -42,14 +36,14 @@ export default withApiAuthRequired(async (
       }
     })
 
-    await auth0ManagementClient.organizations.update({ id: orgId }, {
+    await auth0ManagementClient.organizations.update({ id: organizationId }, {
       metadata: {
         ...(organization.metadata ?? {}),
         [connectionId]: JSON.stringify({ name: connectionName, enabled: true, displayName }),
       },
     })
 
-    await auth0ManagementClient.organizations.addEnabledConnection({ id: orgId }, {
+    await auth0ManagementClient.organizations.addEnabledConnection({ id: organizationId }, {
       connection_id: connectionId,
       assign_membership_on_login: false,
     })
