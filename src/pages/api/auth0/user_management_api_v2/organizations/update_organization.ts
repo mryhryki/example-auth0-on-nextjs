73a7eaa1ec1dff7ef3a1d7ed5116be265a55a310
mryhryki <mryhryki@gmail.com>
2024-09-30 12:02:47 +0900

    Update src/pages/api/auth0/user_management_api_v2/organizations/update_organization.ts

--- src/pages/api/auth0/user_management_api_v2/organizations/update_organization.ts
+++ src/pages/api/auth0/user_management_api_v2/organizations/update_organization.ts
@@ -2,6 +2,7 @@ import { auth0ManagementClient } from '@/utils/auth0/client'
 import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Auth0Organization } from '@/pages/api/auth0/user_management_api_v2/organizations/get_organization'
+import { ApiResponse } from '@/pages/api/auth0/common'
 
 interface ApiResponseData {
   organization: Auth0Organization
@@ -9,14 +10,14 @@ interface ApiResponseData {
 
 export default withApiAuthRequired(async (
   req: NextApiRequest,
-  res: NextApiResponse<ApiResponseData | { error: unknown }>,
+  res: NextApiResponse<ApiResponse<ApiResponseData>>,
 ) => {
   try {
     const session = (await getSession(req, res)) ?? null
     const { metadata } = req.body
     const { data } = await auth0ManagementClient.organizations.update({ id: session?.user.org_id }, { metadata })
-    res.status(200).json({ organization: data })
+    res.status(200).json({ success: true, payload: { organization: data} })
   } catch (err) {
-    res.status(500).json({ error: err })
+    res.status(500).json({ success: false, error: err })
   }
 })
