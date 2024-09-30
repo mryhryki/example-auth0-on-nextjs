244a1c53418f3279674100da4dd79bbe38e7fbbe
mryhryki <mryhryki@gmail.com>
2024-09-30 16:03:40 +0900

    Update src/hooks/useOrganizationMembers.ts

--- src/hooks/useOrganizationMembers.ts
+++ src/hooks/useOrganizationMembers.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { fetchApi } from '@/utils/auth0/api'
 import {
   Auth0OrganizationMemberWithRawUserData,
@@ -7,6 +7,7 @@ import { AppMessage } from '@/components/message/AppMessage'
 
 interface UseOrganizationMembersState {
   members: Auth0OrganizationMemberWithRawUserData[]
+  reload: () => Promise<void>
   loading: boolean;
 }
 
@@ -14,7 +15,8 @@ export const useOrganizationMembers = (): UseOrganizationMembersState => {
   const loading = useRef(true)
   const [members, setMembers] = useState<Auth0OrganizationMemberWithRawUserData[]>([])
 
-  useEffect(() => {
+  const reload = useCallback(async (): Promise<void> => {
+    loading.current = true
     fetchApi<{ members: Auth0OrganizationMemberWithRawUserData[] }>(
       'GET',
       '/auth0/user_management_api_v2/organizations/get_members',
@@ -27,8 +29,13 @@ export const useOrganizationMembers = (): UseOrganizationMembersState => {
     })
   }, [])
 
+  useEffect(() => {
+    reload()
+  }, [reload])
+
   return {
     members,
+    reload,
     loading: loading.current,
   }
 }
