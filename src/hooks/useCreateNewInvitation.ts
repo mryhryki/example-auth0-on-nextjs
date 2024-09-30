ccd7e7d42c9d89551418544327d7324dd430dd13
mryhryki <mryhryki@gmail.com>
2024-09-30 15:18:51 +0900

    Update src/hooks/useCreateNewInvitation.ts

--- src/hooks/useCreateNewInvitation.ts
+++ src/hooks/useCreateNewInvitation.ts
@@ -4,7 +4,7 @@ import { AppMessage } from '@/components/message/AppMessage'
 
 interface FormValues {
   connectionId: string;
-  email: string
+  userId: string
 }
 
 interface UseCreateNewInvitationState {
@@ -15,20 +15,18 @@ interface UseCreateNewInvitationState {
 }
 
 export const useCreateNewInvitation = (): UseCreateNewInvitationState => {
-  const [values, setValues] = useState<FormValues>({ connectionId: '', email: '' })
+  const [values, setValues] = useState<FormValues>({ connectionId: '', userId: '' })
 
-  const canSubmit: boolean = values.connectionId.trim().length > 0 &&
-                             values.email.trim().length > 0 &&
-                             values.email.includes('@')
+  const canSubmit: boolean = values.connectionId.trim().length > 0 && values.userId.startsWith('auth0|')
   const onSubmit = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
     if (!canSubmit) return
     try {
-      const { connectionId, email } = values
+      const { connectionId, userId } = values
       const response = await fetchApi(
         'POST',
         '/auth0/user_management_api_v2/organizations/create_invitation',
-        { connectionId, email },
+        { connectionId, userId },
       )
       AppMessage.addInfoMessage(`Invitation has been created: ${JSON.stringify(response)}`)
     } catch (err) {
