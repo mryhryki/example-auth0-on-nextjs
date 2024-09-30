837c553ce83b775bc538c61228488223e035bdf9
mryhryki <mryhryki@gmail.com>
2024-09-30 14:48:31 +0900

    Update src/pages/api/auth0/user_management_api_v2/organizations/get_organization.ts

--- src/pages/api/auth0/user_management_api_v2/organizations/get_organization.ts
+++ src/pages/api/auth0/user_management_api_v2/organizations/get_organization.ts
@@ -1,7 +1,7 @@
 import { auth0ManagementClient } from '@/utils/auth0/client'
-import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { withApiAuthRequired } from '@auth0/nextjs-auth0'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { ApiResponse } from '@/pages/api/auth0/common'
+import { ApiResponse, getOrganizationId } from '@/pages/api/auth0/common'
 
 export interface Auth0Organization {
   id: string;
@@ -28,8 +28,8 @@ export default withApiAuthRequired(async (
   res: NextApiResponse<ApiResponse<ApiResponseData>>,
 ) => {
   try {
-    const session = (await getSession(req, res)) ?? null
-    const { data } = await auth0ManagementClient.organizations.get({ id: session?.user.org_id })
+    const organizationId = await getOrganizationId(req, res)
+    const { data } = await auth0ManagementClient.organizations.get({ id: organizationId })
     res.status(200).json({ success: true, payload: { organization: data }})
   } catch (err) {
     res.status(500).json({ success: false, error: err })
